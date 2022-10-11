8550e9a59a356afa1e49777fcf1891be118772ee
Karim Mousa <karimmousa202@gmail.comgit config --global --replace-all user.email karimmousa202@gmail.com>
2022-10-11 19:15:36 +0100

    Configure admin interface for users

--- microblogs/admin.py
+++ microblogs/admin.py
@@ -1,3 +1,13 @@
+""" Configuration of administritive interface for microblogs """
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """ Confirguration of the admin interface for users"""
+    list_display =  [
+        'username', 'first_name', 'last_name' , 'email' , 'is_active',
+    ]
+
+    #allows us to specify attribuites that are includded in table of view of users
