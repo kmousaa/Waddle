488f15f7a51fb334f33edcce64d71c54139516a1
Karim Mousa <karimmousa202@gmail.comgit config --global --replace-all user.email karimmousa202@gmail.com>
2022-10-26 17:13:54 +0100

    login form with tests

--- microblogs/forms.py
+++ microblogs/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from django.core.validators import RegexValidator
 from .models import User,Post;
 
+class LogInForm(forms.Form):
+    username = forms.CharField(label='Username')
+    password = forms.CharField(label='Password',widget=forms.PasswordInput())
+
+
 class SignUpForm(forms.ModelForm):
     #allows us to relate form to models
     class Meta:
